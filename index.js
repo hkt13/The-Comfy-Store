9c80b4f8a264169744cb6a058097d8b8c0516f07
Harsh <thakurharsh1112@gmail.com>
2023-07-25 17:57:53 +0530

    path

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ toogle();
      function run()
     {
         console.log('run')
-       if (window.location.pathname === 'https://hkt13.github.io/The-Comfy-Store/index.html') {
+       if (window.location.pathname === '/The-Comfy-Store/index.html') {
         console.log('came here')
            loadingel.style.display = 'block';
     
